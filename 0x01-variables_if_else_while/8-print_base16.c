7d21fceb5753d94e567dfc2eed867abd65404dd5
Soshtakovich <zakessamson1@gmail.com>
2023-03-17 10:47:40 -0700

    C loops and Variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a Hexadecimal String
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char h;
+
+	for (h = '0'; h <= '9'; h++)
+	{
+		putchar(c);
+	}
+
+	for (h = 'a'; h <= 'f'; h++)
+	{
+		putchar(h);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
