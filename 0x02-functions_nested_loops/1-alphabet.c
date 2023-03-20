f19e8931a7c77b53e2dc9859f1dd55dbab33c786
Soshtakovich <zakessamson1@gmail.com>
2023-03-20 09:15:10 -0700

    C Functions and Loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+
+	_putchar('\n');
+}
