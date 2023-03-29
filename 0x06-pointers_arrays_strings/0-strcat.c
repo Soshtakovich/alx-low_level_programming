f2d9ed18fc487ac72c012731642c05ee8d520fe2
Soshtakovich <zakessamson1@gmail.com>
2023-03-29 11:18:38 -0700

    C 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates 2 strings.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+
+	while (*(src + j) != '\0' && j < 97)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	*(dest + j) = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+/**
+ * reverse_array - prints reverse array.
+ * @a: array to be compared.
+ * @n: size of array.
+ * Return: reversed array.
+ */
+void reverse_array(int *a, int n)
+{
+	int swap, strt, lst;
+
+	begin = 0;
+	end = n - 1;
+	while (strt < lst)
+	{
+		swap = *(a + strt);
+		*(a + strt) = *(a + lst);
+		*(a + lst) = swap;
+		strt++;
+		lst--;
+	}
+}
